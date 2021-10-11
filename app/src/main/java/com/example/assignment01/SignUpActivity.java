551254ad1f6c2459ce66defa4033a75a0945de8e
Yubin Park <itqkrdbqls1001@naver.com>
2021-10-11 19:18:02 +0900

    feat: Create signup page and layout

--- app/src/main/java/com/example/assignment01/SignUpActivity.java
+++ app/src/main/java/com/example/assignment01/SignUpActivity.java
@@ -2,15 +2,13 @@ package com.example.assignment01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sign_up);
     }
 }
