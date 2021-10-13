f3e02a64395d2132d05639a6589bd67e0a15167c
Yu-bin Park <itqkrdbqls1001@naver.com>
2021-10-13 14:20:32 +0900

    feat: User info dialog in ProductManagementActivity

--- app/src/main/java/com/example/assignment01/ProductManagementActivity.java
+++ app/src/main/java/com/example/assignment01/ProductManagementActivity.java
@@ -1,9 +1,15 @@
 package com.example.assignment01;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
@@ -24,6 +30,11 @@ public class ProductManagementActivity extends ActivityWithUserInfoView {
     Button deleteCancelBtn;
     Button deleteCommitBtn;
 
+    User loggedUser = null;
+    boolean isGuest = false;
+
+    ProductManagementActivity productManagementActivity = this;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +44,7 @@ public class ProductManagementActivity extends ActivityWithUserInfoView {
 
         Intent intentFromMainActivity = getIntent();
         initializeUserInfo(intentFromMainActivity);
+        isGuest = intentFromMainActivity.getBooleanExtra("isGuest", false);
     }
 
     @Override
@@ -66,16 +78,66 @@ public class ProductManagementActivity extends ActivityWithUserInfoView {
                 deleteModeBottomBtnContainer.setVisibility(View.INVISIBLE);
             }
         });
+
+        userInfoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder dialog = new AlertDialog.Builder(productManagementActivity);
+
+                if (isGuest) {
+                    dialog.setTitle("게스트 사용자");
+                    dialog.setPositiveButton("회원가입", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            Intent intent = new Intent(getApplicationContext(), SignUpActivity.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                    });
+                    dialog.setNegativeButton("돌아가기", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            // DO NOTHING
+                        }
+                    });
+                } else {
+                    dialog.setTitle("사용자 정보");
+
+                    StringBuilder builder = new StringBuilder();
+                    builder.append("ID: " + loggedUser.getId() + "\n");
+
+                    String name = loggedUser.getName().isEmpty() ? "UNKNOWN" : loggedUser.getName();
+                    builder.append("NAME: " + name + "\n");
+
+                    String tel = loggedUser.getTel().isEmpty() ? "UNKNOWN" : loggedUser.getTel();
+                    builder.append("TEL: " + tel + "\n");
+
+                    String addr = loggedUser.getAddress().isEmpty() ? "UNKNOWN" : loggedUser.getAddress();
+                    builder.append("ADDR: " + addr);
+                    dialog.setMessage(builder.toString());
+
+                    dialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            // DO NOTHING
+                        }
+                    });
+                }
+
+                dialog.show();
+            }
+        });
     }
 
     private void initializeUserInfo(Intent intentFromMainActivity) {
         boolean isGuest = intentFromMainActivity.getBooleanExtra("isGuest", true);
 
         if (isGuest) {
+            loggedUser = null;
             Toast.makeText(getApplicationContext(), "게스트 로그인", Toast.LENGTH_SHORT).show();
         } else {
-            User user = intentFromMainActivity.getParcelableExtra("user");
-            Toast.makeText(getApplicationContext(), user.getId(), Toast.LENGTH_SHORT).show();
+            loggedUser = intentFromMainActivity.getParcelableExtra("user");
+            Toast.makeText(getApplicationContext(), loggedUser.getId(), Toast.LENGTH_SHORT).show();
         }
     }
 }
